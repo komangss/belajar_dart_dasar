bb2d97d7dd66dd4f1065a0e551d0ed71819b1631
Antonio Komang Yudistira <komanggt12@gmail.com>
2022-12-25 21:17:26 +0800

    learn comment

---
+++ bin/comment.dart
@@ -0,0 +1,19 @@
+/**
+ * Comment type in Dart
+ * 1. // used for single line comment
+ * 2. /** */ used for multi line comment
+ * 3. /// used for dart standard documentation. the difference with single line comment is, when we generate documentation from Dart program, the will be also included 
+ */
+
+/// this is main function
+/// will executed by dart
+void main() {
+  // this is variable
+  var name = 'anton';
+
+  /**
+   * this is multi line comment
+   * you can add more here
+   */
+  print(name);
+}
