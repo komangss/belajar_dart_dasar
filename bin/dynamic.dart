6cf0c3ef02b3f43d3517c58aaf1815b42513a706
Antonio Komang Yudistira <komanggt12@gmail.com>
2022-12-25 21:44:58 +0800

    learn dynamic data type

---
+++ bin/dynamic.dart
@@ -0,0 +1,14 @@
+void main() {
+  // some times we need a variable that have a flexible data type; 
+  dynamic variable = 100;
+  print(variable);
+
+  variable = "100";
+  print(variable);
+
+  variable = false;
+  print(variable);
+
+  // or you can use
+  var variable2;
+}
