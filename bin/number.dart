f7bf43871f13355d545a6badab57eacb9bf8b481
Antonio Komang Yudistira <komanggt12@gmail.com>
2022-12-25 21:17:33 +0800

    learn number

---
+++ bin/number.dart
@@ -0,0 +1,21 @@
+/**
+ * 2 type of number variable in Dart
+ * Int
+ * Double
+ */
+
+void main() {
+  int number1 = 10;
+  double number2 = 10.5;
+
+  print(number1);
+  print(number2);
+
+  // Num : Flexible number data type in Dart, can be int, can be double
+  num number3 = 10;
+  print(number3);
+
+  number3 = 10.5;
+  print(number3);
+
+}
