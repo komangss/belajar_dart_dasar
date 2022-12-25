7672c6c2647bf1a85bad48c0df5180f32653157c
Antonio Komang Yudistira <komanggt12@gmail.com>
2022-12-25 18:51:28 +0800

    create my first hello world

---
+++ bin/hello_world.dart
@@ -0,0 +1,5 @@
+// same like java/c/kotlin. in dart we use a main function to run a dart project.
+// to run this dart in terminal, input 'dart run bin/hello_world.dart' on your terminal
+void main() {
+  print("Hello World");
+}
