a51db37735e1f42446495499fd797e8f1eedc3bd
Antonio Komang Yudistira <komanggt12@gmail.com>
2022-12-26 21:27:38 +0800

    learn symbol in dart

---
+++ bin/symbol.dart
@@ -0,0 +1,11 @@
+// symbol is data type that rarely be used
+// symbol can actually used for a constant
+
+void main() {
+
+  Symbol symbol1 = Symbol('Antonio Komang');
+  var symbol2 = #belajar_dart;
+
+  print(symbol1);
+  print(symbol2);
+}
