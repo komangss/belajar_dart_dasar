d0636cce4614cdfef18e4a0cc7e23dcb7056af8a
Antonio Komang Yudistira <komanggt12@gmail.com>
2022-12-27 10:52:34 +0800

    learn function parameter

---
+++ bin/function_parameter.dart
@@ -0,0 +1,10 @@
+// we can send/provide information/data 
+// to do that, we can add parameter / argument to the function
+void sayHello(String firstName, String lastName){
+  print('Hello $firstName $lastName');
+}
+
+void main(){
+  sayHello('Antonio', 'Komang');
+  sayHello('Budi', 'Sucita');
+} 
