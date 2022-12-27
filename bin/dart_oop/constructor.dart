79e8d4608ca8ab3671d230a9c2b2fa7b3c9b7f9f
Antonio Komang Yudistira <komanggt12@gmail.com>
2022-12-27 23:07:35 +0800

    learn constructor

---
+++ bin/dart_oop/constructor.dart
@@ -0,0 +1,25 @@
+class Person {
+  String name = 'Guest';
+  String? address;
+  final String country = 'Indonesia';
+
+  // constructor
+  // this will be called when we instantiated
+  // BAD
+  // Person(String name, String address) {
+  //   this.name = name;
+  //   this.address = address;
+  // }
+
+  // GOOD
+  Person(this.name, this.address);
+}
+
+
+void main() {
+  var person1 = Person('Antonio Komang Yudistira', 'Bali');
+
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
+}
