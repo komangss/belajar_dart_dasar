411e61051b068cc614213a1612f4b7c354683dbb
Antonio Komang Yudistira <komanggt12@gmail.com>
2022-12-27 14:52:11 +0800

    learn main function in dart

---
+++ bin/function_main.dart
@@ -0,0 +1,5 @@
+// args ini didapat jika kita menambahkan argument saat run dart ini:
+// dart namafile.dart arg1 arg2 arg3
+void main(List<String> args) {
+  print('Arguments : $args'); // return Arguments : [arg1, arg2] if we use command dart bin/function_main.dart arg1 arg2 
+}
