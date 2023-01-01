f44f464994b3a540cd20f99482a78fc89b506c46
Antonio Komang Yudistira <komanggt12@gmail.com>
2023-01-02 01:54:37 +0800

    Learn Mixin in Dart

---
+++ bin/dart_oop/mixin.dart
@@ -0,0 +1,16 @@
+
+import 'data/multimedia.dart';
+
+void main(){
+
+  var video = Video();
+  video.name = 'Ibot13 RP';
+  video.play();
+  video.stop();
+
+  var audio = Audio();
+  audio.name = 'Tulus';
+  audio.play();
+  audio.stop();
+
+}
