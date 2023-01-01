867b44ac14175b7e2a4a9a2020c9201695135f43
Antonio Komang Yudistira <komanggt12@gmail.com>
2023-01-02 01:56:45 +0800

    Learn Mixin in Dart

--- bin/dart_oop/data/multimedia.dart
+++ bin/dart_oop/data/multimedia.dart
@@ -1,10 +1,11 @@
-
+// topic 2:
+// limiting a mixin
 abstract class Multimedia {
-
 }
 
-mixin Playable on Multimedia {
-
+// topic 1:
+// mixin is reusable code, see video and audio class
+mixin Playable on Multimedia{ // with 'on' keyword, we are limiting playable class can only accessed by class that inherit to it
   String? name;
 
   void play(){
@@ -13,7 +14,7 @@ mixin Playable on Multimedia {
 
 }
 
-mixin Stoppable on Multimedia {
+mixin Stoppable {
 
   String? name;
 
@@ -23,6 +24,11 @@ mixin Stoppable on Multimedia {
 
 }
 
+// with mixin 'with' keyword is used, 
+// we can use multi mixin
+// almost same like encapculation
+
+// class Video with Playable, Stoppable{ // error 'Playable' can't be mixed onto 'Object' because 'Object' doesn't implement 'Multimedia'.
 class Video extends Multimedia with Playable, Stoppable{
 
 }
