e1c0083793d49b8db9547376f28002b4c2acc459
Antonio Komang Yudistira <komanggt12@gmail.com>
2023-01-12 22:25:05 +0800

    learn generic function

---
+++ bin/dart_generic/generic_function.dart
@@ -0,0 +1,9 @@
+import 'helper/array_helper.dart';
+
+void main() {
+  var numbers = [1,2,3,4,5,6,7,8];
+  var names = ['Antonio', 'Komang', 'Yudistira'];
+
+  print(ArrayHelper.count<int>(numbers));
+  print(ArrayHelper.count(names));
+}
