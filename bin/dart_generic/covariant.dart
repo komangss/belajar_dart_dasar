d1a0a7b24d14a7160a9f6d32e0bc2a06626bd059
Antonio Komang Yudistira <komanggt12@gmail.com>
2023-01-13 17:36:31 +0800

    learn Covariant generic in Dart

---
+++ bin/dart_generic/covariant.dart
@@ -0,0 +1,13 @@
+import 'data/mydata.dart';
+
+// covariant mean we can do a subtitute subtype (child) with supertype (parent)
+// be carefull of using covariant because this can make Error on RUNTIME and not detected on compile
+// not recommended to use covariant
+void main() {
+  // this is subtitute from String to the parent Object, this can happen because Object is parent of String
+  MyData<Object> data = MyData<String>("aha");
+  print(data.data);
+
+  // the problem is when we change the data : see below
+  data.data = 100; // THIS WOULD BE ERROR ON RUNTIME
+}
