b2e5d343ada760cb666a202d32146ea5d8fcae1f
Antonio Komang Yudistira <komanggt12@gmail.com>
2023-01-19 22:53:48 +0800

    Created folder to learn unit test on flutter

---
+++ test/dart_unit_test/sample_test.dart
@@ -0,0 +1,14 @@
+import 'package:test/test.dart';
+
+void main() {
+  test("Sample test 1", () {
+    // body unit test
+  });
+  test("Sample test 2", () {
+    // body unit test
+  });
+  test("Sample test 3", () {
+    // body unit test
+  });
+
+}
