7250b9842318100a5e072c78688a505c76e1699d
Antonio Komang Yudistira <komanggt12@gmail.com>
2023-01-23 20:54:28 +0800

    learn verify method is called in dart unit test

--- test/dart_unit_test/mock_object_test.dart
+++ test/dart_unit_test/mock_object_test.dart
@@ -1,5 +1,6 @@
 import 'package:belajar_dart_dasar/book.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test/scaffolding.dart';
 
 @GenerateNiceMocks([MockSpec<BookRepository>()])
@@ -12,6 +13,15 @@ void main() {
 
   test('save book is success', (){
     bookService.save("1", "BookName", 20000);
+
+    // verify 
+    // if we commend save method from bookRepo is called in book serice
+    // the test still pass
+    // why?
+    // because it is just a mock
+    // ALWAYS use verify if u are using Mock object to : Verify the method is called
+    verify(bookRepository.save(Book("1", "BookName", 20000))).called(1); // called make sure the save function is called only 1 times
+    // MAKE SURE u have operator == in the object because mockito will compare the verify method and the executed method to verify. 
   }); 
 
   });
